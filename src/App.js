2bca42d3a82191ca25e128bb1fd8fa1e4ff3fa55
gbxnga <iamblizzyy@gmail.com>
2023-03-15 15:16:11 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+
+import axios from "axios";
+import React, { useMemo, useState, useEffect } from "react";
+
+import Table from "./Table";
+import "./App.css";
 
 function App() {
+  /* 
+    - Columns is a simple array right now, but it will contain some logic later on. It is recommended by react-table to memoize the columns data
+    - Here in this example, we have grouped our columns into two headers. react-table is flexible enough to create grouped table headers
+  */
+
+  const columns = useMemo(
+    () => [
+      {
+        // first group - TV Show
+        Header: "TV Show",
+        // First group columns
+        columns: [
+          {
+            Header: "Time",
+            accessor: "event_block_time",
+          },
+          {
+            Header: "Amount",
+            accessor: "value",
+          },
+        ],
+      },
+      {
+        // Second group - Details
+        Header: "Details",
+        // Second group columns
+        columns: [
+          {
+            Header: "Block Number",
+            accessor: "event_block_number",
+          }, 
+          {
+            Header: "Contract",
+            accessor: "contract_address",
+          }, 
+          {
+            Header: "From",
+            accessor: "from_address",
+          },
+          {
+            Header: "To",
+            accessor: "to_address",
+          },
+        ],
+      },
+    ],
+    []
+  );
+
+  // data state to store the TV Maze API data. Its initial value is an empty array
+  const [data, setData] = useState([]);
+
+  // Using useEffect to call the API once mounted and set the data
+  useEffect(() => {
+    (async () => {
+      const result = await axios("https://dood7td0ik.execute-api.eu-west-1.amazonaws.com/tokens/transfers");
+      console.log({ result })
+      setData(result.data.tokenTransfers);
+    })();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table columns={columns} data={data} />
     </div>
   );
 }
-
 export default App;
